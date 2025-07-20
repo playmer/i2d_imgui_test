da35c7b05bff089e338075e1fc93e47aa9461975
Joshua T. Fisher <playmer@gmail.com>
2025-07-20 16:53:22 -0700

    Fixup ordering of calls, renderdoc...half working?

--- source/app.d
+++ source/app.d
@@ -11,32 +11,43 @@ bindbc.opengl;
 
 import bindbc.imgui.ogl;
 
-extern (Windows)
-int WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow
-) {
-	int result;
+////////////////////////////////////////
+// Commented this out because it disables writing to the console. In C/C++/Cmake I know how to wrangle
+// this so it works when in a debug build and not when in a release build, but whatever, I'm lazy.
+//
+//extern (Windows)
+//int WinMain(
+//	HINSTANCE hInstance,
+//	HINSTANCE hPrevInstance,
+//	LPSTR lpCmdLine,
+//	int nCmdShow
+//) {
+//	int result;
+//
+//	try {
+//		Runtime.initialize();
+//		result = myWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+//		Runtime.terminate();
+//	} catch (Throwable e) {
+//		MessageBoxA(null, e.toString().toStringz(), null, MB_ICONEXCLAMATION);
+//		result = 0; // failed
+//	}
+//
+//	return result;
+//}
 
-	try {
-		Runtime.initialize();
-		result = myWinMain(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
-		Runtime.terminate();
-	} catch (Throwable e) {
-		MessageBoxA(null, e.toString().toStringz(), null, MB_ICONEXCLAMATION);
-		result = 0; // failed
-	}
+void print_gl_version_of_context()
+{
+	int major = 0;
+	int minor = 0;
+	glGetIntegerv(GL_MAJOR_VERSION, &major);
+	glGetIntegerv(GL_MINOR_VERSION, &minor);
 
-	return result;
+	writeln("Major: ", major, " Minor", minor);
+
 }
 
-int myWinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow
+int main(
 ) {
 	loadSDL();
 
@@ -47,23 +58,30 @@ int myWinMain(
         }
     }
 
+	SDL_WindowFlags window_flags = cast(SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE /*| SDL_WINDOW_ALLOW_HIGHDPI*/ );
+	SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+
 	const char* glsl_version = "#version 130";
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
 	// Create window with graphics context
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-	SDL_WindowFlags window_flags = cast(SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE /*| SDL_WINDOW_ALLOW_HIGHDPI*/ );
-	SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+
 	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+
 	SDL_GL_MakeCurrent(window, gl_context);
 	SDL_GL_SetSwapInterval(1); // Enable vsync
 
-	loadOpenGL();
+	auto support = loadOpenGL();
+	writeln("Support: ", support);
+	print_gl_version_of_context();
+
+
 	//InitOpenGLForImGui();
 
 	// Setup Dear ImGui context
